901495266047054c73778b2fe122f7bccfe429f6
Henrik Johansson <henrik.x.johansson@combitech.com>
2023-03-09 13:55:54 +0100

    First commit

---
+++ FileSaverNonSeekableStream/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using CommunityToolkit.Maui.Storage;
+
+namespace FileSaverNonSeekableStream;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private async void OnDownloadClicked(object sender, EventArgs e)
+    {
+        var client = new HttpClient();
+        try 
+        {
+            await using var stream = await client.GetStreamAsync(
+                "https://www.nuget.org/api/v2/package/CommunityToolkit.Maui/5.0.0");
+            var result = await FileSaver.Default.SaveAsync("communitytoolkit.maui.5.0.0.nupkg", stream, CancellationToken.None);
+            if (!result.IsSuccessful) 
+            {
+                await DisplayAlert("Download failed", result.Exception?.Message ?? "Unknown error", "OK");
+            }
+        } 
+        catch (Exception ex) 
+        {
+            await DisplayAlert("Download failed", ex.Message, "OK");
+        }
+    }
+}
+
---
+++ FileSaverNonSeekableStream/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace FileSaverNonSeekableStream;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
